42a1e5cdd63be2b15e835884c1fac2b71fafd1bf
Hoa Giai <damhoagiai456@gmail.com>
2025-05-06 15:18:04 +0700

    update prettierrc

--- E_ReactJS/src/App.jsx
+++ E_ReactJS/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <h3>Push project GitHub</h3>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h3>
+                Push project GitHub
+                <div>askjhdakjs</div>
+            </h3>
+        </div>
+    );
 }
 
 export default App;
